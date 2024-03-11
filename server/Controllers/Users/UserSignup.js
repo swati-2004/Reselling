c9b1be5fbd0b8aee539dd81be21bd7400706806d
vipul1435 <vipulkumarraj2003@gmail.com>
2024-03-12 01:38:11 +0530

    All Validators Functions are Completed

--- server/Controllers/Users/UserSignup.js
+++ server/Controllers/Users/UserSignup.js
@@ -1,21 +1,25 @@
-import User from "../../Models/UserSchema.js";
-import validateEmail from "../../Utils/Validators/emailValidator.js";
+// import User from "../../Models/UserSchema.js";
+// import validateEmail from "../../Utils/Validators/Validate.js";
+import Validators from "../../Utils/Validators/Validate.js";
+console.log(Validators.ValidateName("Rahul").success);
 
-const UserSingup = async(req,res)=>{
-    try {
-        let UserData = req?.body;
-        let {name, email, mobile, password, state, district,pincode} = UserData;
-        console.log(UserData);
-        ;(()=>{
-            if(!name || !email || !mobile || !password || !state || !district || !pincode){
-                return res.status(400).json({success:false,message:"All fields are required"});
-            }
+// const UserSingup = async(req,res)=>{
+//     try {
+//         let UserData = req?.body;
+//         let {name, email, mobile, password, state, district,pincode} = UserData;
+//         console.log(UserData);
+//         ;(()=>{
+//             if(!name || !email || !mobile || !password || !state || !district || !pincode){
+//                 return res.status(400).json({success:false,message:"All fields are required"});
+//             }
             
             
-        })();
-    } catch (error) {
+//         })();
+//     } catch (error) {
         
-    }
-}
+//     }
+// }
 
-export default UserSingup;
+// export default UserSingup;
+
+// console.log(Validators);
